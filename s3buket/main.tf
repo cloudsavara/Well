750c784f0e56b0016539c3dead50a43d67c516da
ramee2 <rizi.ameer15@gmail.com>
2021-05-13 11:49:21 -0500

    added code to create S3 bucket with Version, log, KMS and tag using terraform

---
+++ s3buket/main.tf
@@ -0,0 +1,5 @@
+module s3_bucket {
+  source = "github.com/ramee2/Well/s3bucket/s3bucket"
+  bucket_prefix = "this-is-only-a-test-bucket-delete-assignment"
+  target_bucket = "this-is-the-target-bucket-assignment"
+}
